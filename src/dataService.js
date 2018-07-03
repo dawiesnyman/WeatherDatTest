619d87e00985524c4b7de35a6d2d27b776b0ba09
Dawie Snyman <dawie@dvelop.co.za>
2018-07-03 15:09:51 +1200

    first commit

---
+++ src/dataService.js
@@ -0,0 +1,23 @@
+
+
+var DataService = function(){
+	//this.file = file;
+	this.data = "";
+}
+
+DataService.prototype.readData = (inputFile) => {
+	const temporaryFileReader = new FileReader();
+
+	return new Promise((resolve, reject) => {
+		temporaryFileReader.onerror = () => {
+			temporaryFileReader.abort();
+			reject(new DOMException("Problem parsing input file."));
+		};
+
+		temporaryFileReader.onload = () => {
+			resolve(temporaryFileReader.result);
+		};
+		
+		temporaryFileReader.readAsText(inputFile);
+		});
+};
